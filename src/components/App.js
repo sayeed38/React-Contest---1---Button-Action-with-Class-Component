3a6478e8645bdd677d9da328362f917ba223360b
sayeed38 <mohammedsayeed1997@gmail.com>
2020-10-21 21:41:05 +0530

    Modified Changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,18 @@ class App extends Component {
     constructor(props) {
 		super(props);
 		this.state = {para : ""};
+		this.handleClick = () => {
+			this.setState({para : "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"});
+		}
 	};
 	
-	const handleClick = () => {
-		this.setState({para : "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"});
-	}
+	
 
     render() {
     	return(
     		<div id="main">
 				<button id="click" onClick={this.handleClick}>Click</button>
-					{this.state.para != "" ? <p id="para">{this.state.para}</p> : null}
+					{this.state.para !== "" ? <p id="para">{this.state.para}</p> : null}
     		</div>
     	);
     }
